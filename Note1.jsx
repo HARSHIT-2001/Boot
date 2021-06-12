95a6b30aae5a2443eed252a49272a8da21dd5dfd
HARSHIT-2001 <77965414+HARSHIT-2001@users.noreply.github.com>
2021-06-12 21:33:32 +0530

    Add files via upload

---
+++ Note1.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note1() {
+  return (
+    <div className="note">
+      <h1>Hoorey...</h1>
+      <p>
+        That was amazing bootcamp absolutely Awesome...I have learnt a lot ...
+      </p>
+    </div>
+  );
+}
+
+export default Note1;
